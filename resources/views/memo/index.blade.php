f7bc00446c4b13a391502c4abe9ee0a4d021b2fa
seiichi0404 <yamayamaseisei1834@gmail.com>
2021-06-17 13:48:30 +0900

    bootstrap導入

--- resources/views/memo/index.blade.php
+++ resources/views/memo/index.blade.php
@@ -1,1 +1,21 @@
-こちらメモ投稿画面です
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <button type="button" class="btn btn-primary">Primary</button>
+                <button type="button" class="btn btn-secondary">Secondary</button>
+                <button type="button" class="btn btn-success">Success</button>
+                <button type="button" class="btn btn-danger">Danger</button>
+                <button type="button" class="btn btn-warning">Warning</button>
+                <button type="button" class="btn btn-info">Info</button>
+                <button type="button" class="btn btn-light">Light</button>
+                <button type="button" class="btn btn-dark">Dark</button>
+                <button type="button" class="btn btn-link">Link</button>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
