9bb8c99b2ef89fc28cf647adb983238133c2a7f7
seiichi0404 <yamayamaseisei1834@gmail.com>
2021-07-23 08:57:37 +0900

    メモロック解除ユースケース切り分け

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -17,6 +17,7 @@ use App\UseCase\UseMemo\MemoUpdateUseCase;
 use App\UseCase\UseMemo\MemoDestroyUseCase;
 use App\UseCase\UseMemo\MemoLockUseCase;
 use App\UseCase\UseMemo\MemoLockCloseUseCase;
+use App\UseCase\UseMemo\MemoLockReleaseUseCase;
 
 class MemoController extends Controller
 {
@@ -96,32 +97,10 @@ class MemoController extends Controller
     }
 
     // メモロック 解除
-    public function memo_lock_release(Request $request){
+    public function memo_lock_release(Request $request, MemoLockReleaseUseCase $memo){
 
 
-        // 認証ユーザーの取得
-        $user = Auth::user();
-
-        // 選択されたメモid取得
-        $memo_id = $request->input('memo_id');
-
-        // 入力されたパスワード取得
-        $input_password = $request->input('memo_password');
-
-        // パスワード認証チェック
-        if (Hash::check($input_password, $user->password)){
-            // 認証成功
-            DB::table('memos')
-            ->where('memo_id', $memo_id)
-            ->update(['key_lock_status'=> 'true']);
-        }
-
-        // 選択中メモ セッション更新
-        $memo = DB::table('memos')
-        ->where('memo_id', $memo_id)
-        ->first();
-
-        session()->put('select_memo', $memo);
+        $memo->memoRelease($request);
 
         return redirect()->route('memo.index');
 
---
+++ app/UseCase/UseMemo/MemoLockReleaseUseCase.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\UseCase\UseMemo;
+
+use Illuminate\Http\Request;
+use App\Models\Folder;
+use App\Models\Memo;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\Model;
+use App\Services\GetNewMemo;
+
+Class MemoLockReleaseUseCase {
+    public function memoRelease($request){
+
+        // 認証ユーザーの取得
+        $user = Auth::user();
+
+        // 選択されたメモid取得
+        $memo_id = $request->input('memo_id');
+
+        // 入力されたパスワード取得
+        $input_password = $request->input('memo_password');
+
+        // パスワード認証チェック
+        if (Hash::check($input_password, $user->password)){
+            // 認証成功
+            DB::table('memos')
+            ->where('memo_id', $memo_id)
+            ->update(['key_lock_status'=> 'true']);
+        }
+
+        // 選択中メモ セッション更新
+        $memo = DB::table('memos')
+        ->where('memo_id', $memo_id)
+        ->first();
+
+        session()->put('select_memo', $memo);
+    }
+}
