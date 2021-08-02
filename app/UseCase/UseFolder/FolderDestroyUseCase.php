8956a2076ea881984a6055da4566e5c2a352e593
seiichi0404 <yamayamaseisei1834@gmail.com>
2021-08-03 08:36:12 +0900

    フォルダ削除時は全ファイルフォルダの選択

--- app/UseCase/UseFolder/FolderDestroyUseCase.php
+++ app/UseCase/UseFolder/FolderDestroyUseCase.php
@@ -13,5 +13,12 @@ Class FolderDestroyUseCase {
         $folder = DB::table('folders')
         ->where('folder_id', $request->id)
         ->delete();
+
+        // 親フォルダの取得
+        $parent_folder = session()->get('parent_folder');
+
+        // セッション削除 (削除後全ファイルフォルダの選択)
+        session()->forget('parent_folder');
+        session()->forget('select_folder');
     }
 }
