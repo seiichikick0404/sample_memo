cd49c659381bd7e1cdc5bd4c1bd722ee0a6a29d3
seiichi0404 <yamayamaseisei1834@gmail.com>
2021-08-03 08:53:25 +0900

    フォルダ更新時メモセッション更新

--- app/UseCase/UseFolder/FolderUpdateUseCase.php
+++ app/UseCase/UseFolder/FolderUpdateUseCase.php
@@ -13,8 +13,6 @@ Class FolderUpdateUseCase {
     public function folderUpdate($request){
         $folder = new Folder;
 
-        // dd($request);
-
         $int_edit_id = $request['folder_id'];
         // 数値に変換
         $edit_id = (int) $int_edit_id;
@@ -24,6 +22,21 @@ Class FolderUpdateUseCase {
         $folder = DB::table('folders')
         ->where('folder_id', $edit_id)
         ->update(['folder_name'=> $edit_name]);
+
+        // 更新フォルダ取得
+        $session_folder = DB::table('folders')
+        ->where('folder_id', $edit_id)
+        ->first();
+
+        // 更新フォルダに属するメモ取得
+        $session_memo = DB::table('memos')
+        ->where('folder_id', $edit_id)
+        ->orderBy('created_at', 'desc')
+        ->first();
+
+        // セッション更新
+        session()->put('select_folder', $session_folder);
+        session()->put('select_memo', $session_memo);
 
     }
 }
