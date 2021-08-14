c3c4cdcb55b933f22799649c5e83e6de47a1b707
seiichi0404 <yamayamaseisei1834@gmail.com>
2021-08-14 09:02:48 +0900

    ログイン処理修正

--- app/UseCase/UseMemo/MemoUpdateUseCase.php
+++ app/UseCase/UseMemo/MemoUpdateUseCase.php
@@ -10,9 +10,6 @@ Class MemoUpdateUseCase {
 
     public function memoUpdate($request){
 
-        // dd($request);
-
-
         $id = $request['edit_id'];
         $title = $request['edit_title'];
         $content = $request['edit_content'];
@@ -21,6 +18,7 @@ Class MemoUpdateUseCase {
         DB::table('memos')
         ->where('memo_id', $id)
         ->update(['title'=> $title, 'text'=> $content]);
+
 
         $update_memo = DB::table('memos')
         ->where('memo_id', $id)
